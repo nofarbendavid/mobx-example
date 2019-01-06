fb6bdd21d90ceccfd8ea2a46ae0a52a8f2110bf3
ronlanger <langer.ron.dev@gmail.com>
2019-01-06 16:25:27 +0200

    test precommit hook

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,22 +1,22 @@
 import React, { Component } from 'react';
-import {inject, observer} from "mobx-react";
-import {Stores} from "../stores";
-
+import { inject, observer } from 'mobx-react';
+import { Stores } from '../stores';
 
 type Props = {
-  startNetwork?:                  (label: string) => void,
-  endNetwork?: (label: string) => void,
-  networkCount?: number
-}
+  startNetwork?: (label: string) => void;
+  endNetwork?: (label: string) => void;
+  networkCount?: number;
+};
 
-@inject(({ networkStore }: Stores): Props => ({
-  networkCount: networkStore.getByLabel('general'),
-  startNetwork: networkStore.startNetwork,
-  endNetwork: networkStore.endNetwork
-}))
+@inject(
+  ({ networkStore }: Stores): Props => ({
+    networkCount: networkStore.getByLabel('general'),
+    startNetwork: networkStore.startNetwork,
+    endNetwork: networkStore.endNetwork
+  })
+)
 @observer
 class App extends Component<Props> {
-
   fireRequest = () => {
     if (this.props.startNetwork) {
       this.props.startNetwork('general');
@@ -31,23 +31,23 @@ class App extends Component<Props> {
 
   render() {
     return (
-        <div className="App">
-          <header className="App-header">
-            <p>
-              Edit <code>src/App.tsx</code> and save to reload.
-            </p>
-            NetworkCount: { this.props.networkCount }
-            <button onClick={this.fireRequest}>Fire</button>
-            <button onClick={this.endRequest}>End</button>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer">
-              Learn React
-            </a>
-          </header>
-        </div>
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          NetworkCount: {this.props.networkCount}
+          <button onClick={this.fireRequest}>Fire</button>
+          <button onClick={this.endRequest}>End</button>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer">
+            Learn React
+          </a>
+        </header>
+      </div>
     );
   }
 }
